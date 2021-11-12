5c91c56b9de3b986dc5a590f3a2e90a9c59403bf
Johnson Zhou <johnson.zhou.sh@icloud.com>
2021-11-11 23:01:49 -0800

    Added jsdoc for groupme & others
    Added jsdoc for groupme & others
    Added config.js for configuration
    Refactored and fixed mutation observer

--- README.md
+++ README.md
@@ -10,33 +10,50 @@ To add the extension to your Chrome browser:
 
 Other resources [debugging tutorial](https://developer.chrome.com/docs/extensions/mv3/tut_debugging/)
 
+
+## JSDoc
+
+To generate JSDocs
+
+```shell
+npm install -g jsdoc
+jsdoc *.js
+```
+
+## Directory structure
+
 Bruin-Assistant-Extension     
-├─ assets                     
-│  └─ groupme.png             
-├─ tests                      
-│  ├─ map-backend             
-│  │  ├─ imgs                 
-│  │  │  ├─ test1_result.jpg  
-│  │  │  ├─ test1_succ.jpg    
-│  │  │  ├─ test2_result.jpg  
-│  │  │  ├─ test2_succ.jpg    
-│  │  │  ├─ test3_result.jpg  
-│  │  │  └─ test3_succ.jpg    
-│  │  ├─ README.md            
-│  │  ├─ test1.js             
-│  │  ├─ test2.js             
-│  │  └─ test3.js             
-│  └─ README.md               
-├─ background.js              
-├─ bruinwalk.js               
-├─ groupme.css                
-├─ groupme.js                 
-├─ jquery-3.6.0.min.js        
-├─ LICENSE                    
-├─ manifest.json              
-├─ observer.js                
-├─ README.md                  
-├─ settings.css               
-├─ settings.html              
-├─ settings.js                
-└─ timedistance.js
+├── LICENSE
+├── README.md
+├── assets
+│   └── groupme.png
+├── background.js
+├── bruinwalk.js
+├── config.js
+├── groupme.css
+├── groupme.js
+├── jquery-3.6.0.min.js
+├── manifest.json
+├── observer.js
+├── package.json
+├── settings.css
+├── settings.html
+├── settings.js
+├── tests
+│   ├── README.md
+│   ├── groupme
+│   │   └── unittest.js
+│   ├── map-backend
+│   │   ├── README.md
+│   │   ├── imgs
+│   │   │   ├── test1_result.jpg
+│   │   │   ├── test1_succ.jpg
+│   │   │   ├── test2_result.jpg
+│   │   │   ├── test2_succ.jpg
+│   │   │   ├── test3_result.jpg
+│   │   │   └── test3_succ.jpg
+│   │   ├── test1.js
+│   │   ├── test2.js
+│   │   └── test3.js
+│   └── mutation
+└── timedistance.js
--- observer.js
+++ observer.js
@@ -8,17 +8,26 @@
 
 let timeout = null
 
-// DOM Nodes that should not trigger mutation callback for reload.
-const observerWhiteListClassName = [
-    "inst-rating-title-div",
-    "inst-rating-content-div",
-    "hide-small",
-    "section-header",
-    "inst-rating-footer-div",
-];
-
+/**
+ * The list of ClassNames that are expected to be injected into the DOM. 
+ * 
+ * @see {@link removePrevInjections} for removing injections from DOM.
+ * @see {@link validMutation} for checking if the child nodes added are in the 
+ * whitelist of injections.
+ */
 const injectionClassNames = ["th-timedistance", "td-timedistance", "qr-popup"];
 
+
+/**
+ * Checks whether the mutation is caused by extension injection, if so then do
+ * not reload. If this is not prevented, we'll have an infinite loop of reloads.
+ * 
+ * @function 
+ * 
+ * @param {MutationRecord} mutation 
+ * @param {*} callback 
+ * @returns {void}
+ */
 function validMutation(mutation, callback) {
     for (let node of mutation.addedNodes) {
         if (!injectionClassNames.includes(node.className)) {
@@ -28,6 +37,14 @@ function validMutation(mutation, callback) {
         }
     }
 }
+
+/**
+ * Creates a mutation observers that listens for DOM mutations. 
+ * 
+ * @global 
+ * @constant {MutationObserver}
+ * @readonly
+ */
 
 const observer = new MutationObserver((mutationsList, observer) => {
     for (const mutation of mutationsList) {
@@ -44,9 +61,21 @@ const observer = new MutationObserver((mutationsList, observer) => {
 
 const config = { subtree: true, childList: true };
 
-observer.observe(document.getElementsByClassName("classPlanner_ClassesInPlanSection")[0], config);
+// Attaches the MutationObserver onto the 
+observer.observe(document, config);
 
-// this is called whenever DOM is modifies
+/**
+ * This is the callback for Mutation Observer that listens on class planner 
+ * changes in the DOM.
+ * 
+ * @function 
+ * 
+ * @param {*} mutationsList 
+ * @param {*} observer 
+ * 
+ * @returns {void}
+ */
+// 
 function repopulate(mutationsList, observer) {
     // run the script if it detects a class search page
     // Need to remove previous injections to avoid duplicates
@@ -59,9 +88,36 @@ function repopulate(mutationsList, observer) {
     }
 }
 
+/**
+ * Removes previous injections to prepare to repopulate injections.
+ * 
+ * @function
+ * @returns {void}
+ * 
+ * @see {@link injectionClassNames} for removing previous injections.
+ * @see {@link repopulate} to find use case.
+ */
 function removePrevInjections() {
     for (const className of injectionClassNames) {
         const elements = document.getElementsByClassName(className);
         while (elements.length > 0) elements[0].remove();
+    }
+}
+
+/**
+ * Resize the column width of the course table to make things look nicer.
+ * 
+ * This function is invoked whenever the extension injections happen.
+ * 
+ * @function
+ * @returns {void}
+ */
+function resizeColumnWidths() {
+
+    for (const table of document.getElementsByClassName('coursetable')) {
+        if (table.getElementsByTagName('th').length >= 6) {
+            table.getElementsByTagName('th')[1].style.width = "8%"; // Section
+            table.getElementsByTagName('th')[5].style.width = "13%"; // Time
+        }
     }
 }
