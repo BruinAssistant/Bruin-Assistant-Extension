7da6da10b8406a3ea587872f534b3772d44d1dc2
Eldon Ngo <40747060+eldon3141@users.noreply.github.com>
2021-11-11 19:28:46 -0800

    Update timedistance.js

--- timedistance.js
+++ timedistance.js
@@ -1,8 +1,8 @@
 // This will use the same observer from groupme.js
 observer.observe(document.querySelector('.centerColumn'), config);
 
-prepTimeDistance();
-
+// prepTimeDistance();
+initiateTimeDistance();
 function test(response){
     console.log(response);
 }
@@ -16,15 +16,15 @@ function getLng(building){
 }
 
 // TODO 
-function populateTimeDistance(response, ordered_buildings){
+function populateTimeDistance(response, ordered_classes){
     console.log(response);
-    console.log(ordered_buildings);
+    console.log(ordered_classes);
 
-    let matrix = response.rows;
+    let td_matrix = response.rows;
 }
 
 
-function prepTimeDistance(){
+function initiateTimeDistance(){
     function Coord(lat, lng){
         this.lat = lat;
         this.lng = lng;
@@ -39,16 +39,15 @@ function prepTimeDistance(){
         ["Royce Hall", new Coord(34.072924325772995, -118.44218065821016)],
     ])
 
-    let building_info = getClassBuildings();
-    let ordered_buildings = building_info.get("orderedBuildings");
+    let class_info = getClassBuildings();
 
     // Construct unique building coordinates by indexing into coordinate table
     let unique_buildings = [];
-    for (let building of building_info.get("uniqueBuildings")){
+    for (let building of class_info.get("uniqueBuildings")){
         if(building != "Online")
             unique_buildings.push(coords.get(building));
     }
-    console.log(unique_buildings);
+
     let total_buildings = unique_buildings.length;
     let str_coord = "";
     let latStr = "";
@@ -68,14 +67,14 @@ function prepTimeDistance(){
     }   
 
     // TODO: SECURE API_KEY 
-    var map_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + str_coord + "&" + "destinations=" + str_coord + "&units=imperial&mode=walking&key=";
+    var map_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + str_coord + "&" + "destinations=" + str_coord + "&units=imperial&mode=walking&key=AIzaSyDW_FKptRQMGtYiQqPPw4s15jkF-YLe2w8";
     
     chrome.runtime.sendMessage({
         url: map_url,
         contentScriptQuery: "getTimeDistanceData"
     }
         , response => {
-            populateTimeDistance(response,ordered_buildings);
+            populateTimeDistance(response, class_info.get("orderedClasses"));
         }
     );
 
@@ -83,6 +82,7 @@ function prepTimeDistance(){
 /* 
     This return a 2-element array.
 
+    TODO: UPDATE THIS COMMENT
     1st element: A mapping from day of week to an ordered list
     of buildings that the student must visit for that day.
 
@@ -91,7 +91,12 @@ function prepTimeDistance(){
 */
 function getClassBuildings() {
     const result = new Map();
-    const ordered_buildings = new Map();
+    function classInfo(name, section, building){
+        this.name = name;
+        this.section = section;
+        this.building = building; 
+    };
+    const ordered_classes = new Map();
     const week_boxes = document.getElementsByClassName('timebox');
     const day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
     const unique_buildings = new Set();
@@ -99,20 +104,26 @@ function getClassBuildings() {
 
     for (const day_boxes of week_boxes) {
         let class_boxes = day_boxes.getElementsByClassName('planneritembox');
-        let buildings = [];
+        let classArr = [];
         for(const class_box of class_boxes){
-            let place = class_box.getElementsByClassName('hide-above-small')[1].nextSibling.data;
+            let class_info = class_box.innerText.split("\n");
+            if(class_info.length != 3){
+                console.log("Error: Parsed class info is not length 3.");
+            }
+            let name = class_info[0];
+            let section = class_info[1];
+            let place = class_info[2];
             let building = extractBuilding(place);
-            buildings.push(building);
+            classArr.push(new classInfo(name, section, building));
             unique_buildings.add(building);
         }
-        ordered_buildings.set(day[i],buildings);
+        ordered_classes.set(day[i], classArr);
         i++;
     }
     
-    result.set("orderedBuildings", ordered_buildings);
+    result.set("orderedClasses", ordered_classes);
     result.set("uniqueBuildings", unique_buildings);
-
+    console.log(result);
     return result;
 }
 
